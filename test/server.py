3ba0d5e8a5eea6349999037ba72d381ff2051a03
Manuel García <manuelg3265@gmail.com>
2021-11-03 12:19:56 -0500

    File

--- test/server.py
+++ test/server.py
@@ -280,6 +280,14 @@ async def recognize(ws: WebSocket):
                 gSem_acquired = False
             break
 
+@app.websocket('/')
+async def healthcheck(ws: WebSocket):
+    await ws.accept()
+    """Route for simple healthcheck that simply returns greeting message"""
+    await ws.send_json({
+        "api": 'Vozy STT',
+        "status": "200"
+    })
 
 print('Running in the port: ' + str(6000) + '. Environment: ')
 uvicorn.run(app, host="0.0.0.0", port=6000)
