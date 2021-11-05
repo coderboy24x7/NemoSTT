b51c91648f44ccdb6b1f2825f9e457d6d22c1ab1
Manuel García <manuelg3265@gmail.com>
2021-11-05 11:33:40 -0500

    Parallel

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ import pytorch_lightning as pl
 cont = 0
 metadata = open('/home/csanta/lili-asr/dataset/dev_trans.json')
 quartznet = nemo_asr.models.EncDecCTCModel.from_pretrained(model_name="stt_es_quartznet15x5")
-total_lines = len(metadata.read())
 for line in metadata:
         json_load = json.loads(line)
         filename = json_load['audio_filepath'].split('/')[-1]
@@ -13,5 +12,5 @@ for line in metadata:
         inference = quartznet.transcribe(paths2audio_files=[os.path.join('/home/csanta/lili-asr/dataset/test/', filename)])
         error = wer(text, inference)
         cont = cont + error
-
+total_lines = len(metadata.read())
 print('Final WER:', cont/total_lines)
