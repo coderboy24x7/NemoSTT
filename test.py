0bcd9bcfa27b174d864738aab2df41fd6b9abc5a
Manuel García <manuelg3265@gmail.com>
2021-11-05 10:45:22 -0500

    Parallel

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-import os
-from nemo.collections.asr.models import ASRModel
-
-# Iterate for each dict object in os.walk()
+import os, copy, torch
+import nemo.collections.asr as nemo_asr
+import pytorch_lightning as pl
 for root, dirs, files in os.walk("/home/csanta/lili-asr/dataset/test/"):
-        print(type(files))
-        # asr_model = ASRModel.from_pretrained(model_name=cfg.pretrained_name, map_location=device)  # type: ASRModel
-        # model_name = cfg.pretrained_name
+        for file in files:
+                if file.endswith('.wav'):
+                        quartznet = nemo_asr.models.EncDecCTCModel.from_pretrained(model_name="stt_es_quartznet15x5")
+                        print(quartznet.transcribe(paths2audio_files=os.path.join(root, file)))
