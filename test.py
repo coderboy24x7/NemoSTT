92adedcf0fcca7a223f286b29f9fa274cd304cc4
Manuel García <manuelg3265@gmail.com>
2021-11-05 11:12:02 -0500

    Parallel

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ import pytorch_lightning as pl
 metadata = open('/home/csanta/lili-asr/dataset/jsons/dev_trans.json')
 quartznet = nemo_asr.models.EncDecCTCModel.from_pretrained(model_name="stt_es_quartznet15x5")
 for line in metadata:
-        print(json.dumps(line))
+        print(json.loads(line))
                         # print(quartznet.transcribe(paths2audio_files=[os.path.join(root, file)]))
