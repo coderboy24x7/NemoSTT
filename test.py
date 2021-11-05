278a33208bab07aec498fb3f71daf2e32720b816
Manuel García <manuelg3265@gmail.com>
2021-11-05 10:24:45 -0500

    Parallel

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import os
+from nemo.collections.asr.models import ASRModel
 
 # Iterate for each dict object in os.walk()
 for root, dirs, files in os.walk("/home/csanta/lili-asr/dataset/test/"):
-        print(files)
+        print(type(files))
+        # asr_model = ASRModel.from_pretrained(model_name=cfg.pretrained_name, map_location=device)  # type: ASRModel
+        # model_name = cfg.pretrained_name
