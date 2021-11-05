5814fd4bad89b11a1431b1645405cc51cdf21e37
Manuel García <manuelg3265@gmail.com>
2021-11-05 11:31:34 -0500

    Parallel

--- test.py
+++ test.py
@@ -2,13 +2,16 @@ import os, copy, torch, json
 from jiwer import wer
 import nemo.collections.asr as nemo_asr
 import pytorch_lightning as pl
-metadata = open('/home/csanta/lili-asr/dataset/jsons/dev_trans.json')
-# quartznet = nemo_asr.models.EncDecCTCModel.from_pretrained(model_name="stt_es_quartznet15x5")
-print(len(metadata.read()))
+cont = 0
+metadata = open('/home/csanta/lili-asr/dataset/dev_trans.json')
+quartznet = nemo_asr.models.EncDecCTCModel.from_pretrained(model_name="stt_es_quartznet15x5")
+total_lines = len(metadata.read())
 for line in metadata:
         json_load = json.loads(line)
-#         filename = json_load['audio_filepath'].split('/')[-1]
-#         text = json_load['text']
-#         inference = quartznet.transcribe(paths2audio_files=[os.path.join('/home/csanta/lili-asr/dataset/test/', filename)])
-#         error = wer(text, inference)
-#         print(error)
+        filename = json_load['audio_filepath'].split('/')[-1]
+        text = json_load['text']
+        inference = quartznet.transcribe(paths2audio_files=[os.path.join('/home/csanta/lili-asr/dataset/test/', filename)])
+        error = wer(text, inference)
+        cont = cont + error
+
+print('Final WER:', cont/total_lines)
