f143ae8f562cd26d82f06e13b2471c81d7f8c59b
Manuel García <manuelg3265@gmail.com>
2021-11-05 10:48:38 -0500

    Parallel

--- test.py
+++ test.py
@@ -2,8 +2,7 @@ import os, copy, torch
 import nemo.collections.asr as nemo_asr
 import pytorch_lightning as pl
 for root, dirs, files in os.walk("/home/csanta/lili-asr/dataset/test/"):
-        print(root, dirs)
-        # for file in files:
-        #         if file.endswith('.wav'):
-        #                 quartznet = nemo_asr.models.EncDecCTCModel.from_pretrained(model_name="stt_es_quartznet15x5")
-        #                 print(quartznet.transcribe(paths2audio_files=os.path.join(root, file)))
+        for file in files:
+                if file.endswith('.wav'):
+                        quartznet = nemo_asr.models.EncDecCTCModel.from_pretrained(model_name="stt_es_quartznet15x5")
+                        print(quartznet.transcribe(paths2audio_files=[os.path.join(root, file)]))
